acbabf2676fe95ce9a4303bf2af5905ed87b3aef
Nazilah17 <nazilah0000@gmail.com>
2024-01-15 16:00:54 +0800

    edit

--- app.js
+++ app.js
@@ -9,15 +9,6 @@ const { authenticateToken } = require("./middlewares/middleware");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-db.sequelize
-  .sync({ alter: true })
-  .then(() => {
-    console.log("Database synced");
-  })
-  .catch((err) => {
-    console.error("Error syncing database:", err);
-  });
-
 app.get("/", (req, res) => {
   res.json({
     message: "Welcome to My ToDoList API",
